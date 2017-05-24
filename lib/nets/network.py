93d13a834f4d43f31706a0443aa11f1f09e808e3
Markus Nagel <mail@markusnagel.com>
2017-05-24 11:42:16 +0200

    Added option that a network can get an image and im_info tensor as input (instead of creating placeholders). This is required if there should be some additonal custom transformation in the graph before.

--- lib/nets/network.py
+++ lib/nets/network.py
@@ -287,9 +287,14 @@ class Network(object):
     return loss
 
   def create_architecture(self, sess, mode, num_classes, tag=None,
-                          anchor_scales=(8, 16, 32), anchor_ratios=(0.5, 1, 2)):
-    self._image = tf.placeholder(tf.float32, shape=[self._batch_size, None, None, 3])
-    self._im_info = tf.placeholder(tf.float32, shape=[self._batch_size, 3])
+                          anchor_scales=(8, 16, 32), anchor_ratios=(0.5, 1, 2),
+                          image=None, im_info=None):
+    if image is not None and im_info is not None:
+      self._image = image
+      self._im_info = im_info
+    else:
+      self._image = tf.placeholder(tf.float32, shape=[self._batch_size, None, None, 3])
+      self._im_info = tf.placeholder(tf.float32, shape=[self._batch_size, 3])
     self._gt_boxes = tf.placeholder(tf.float32, shape=[None, 5])
     self._tag = tag
 
